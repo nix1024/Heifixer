687662af626e8bb6d59f370f1a83bc53f33ef44f
Nix Wang <nix1024@gmail.com>
2026-04-20 17:28:02 +0800

    Initial Commit

---
+++ Heifixer/HeifixerApp.swift
@@ -0,0 +1,32 @@
+//
+//  HeifixerApp.swift
+//  Heifixer
+//
+//  Created by 王昕 on 2026/4/20.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HeifixerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ HeifixerTests/HeifixerTests.swift
@@ -0,0 +1,18 @@
+//
+//  HeifixerTests.swift
+//  HeifixerTests
+//
+//  Created by 王昕 on 2026/4/20.
+//
+
+import Testing
+
+struct HeifixerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
